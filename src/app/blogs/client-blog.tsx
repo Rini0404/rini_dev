26e7fa31e8e5c13b842c4c2c07ca0faee601c7cd
Rini <Reneortega2468@gmail.com>
2023-11-25 16:01:28 -0500

    Added code from mac mini

--- src/app/blogs/client-blog.tsx
+++ src/app/blogs/client-blog.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import { motion } from "framer-motion";
 import { BlogsArray } from "./blog-types";
@@ -9,73 +8,59 @@ type BlogsProps = {
 };
 
 const Blogs: React.FC<BlogsProps> = ({ data }) => {
-  console.log('Data: ', data)
   return (
-    <div className="flex flex-col justify-center items-start pt-4 gap-4 px-4 w-3/5 mx-auto">
-    {/* Your Personal Image, Name, and Date */}
-    <div className="flex items-center gap-4 mb-8">
-      <img src= {"https://miro.medium.com/v2/resize:fill:96:96/1*9aVMsOsEQ1-YIa5LS8EHTQ@2x.jpeg"}alt="Your Name" className="w-16 h-16 rounded-full " />
-      <div>
-        <h2 className="text-xl  text-color font-bold">Rene Ortega</h2>
-      </div>
-    </div>      
-    {data.map((blog, index) => (
-        <motion.div
+    <div className="flex flex-col justify-center items-start py-20 gap-4 px-4 w-full max-w-[80%] mx-auto">
+      {/* Personal Image, Name, and Date */}
+      {data.map((blog) => (
+        <a
           key={blog.id}
-          className="w-full p-4 bg-slate-800 rounded-xl shadow-lg transition-all duration-200 flex items-center border-b border-slate-700"
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          whileHover={{ scale: 1.02, boxShadow: "0 2px 10px rgba(0, 255, 170, 0.637)" }}
-          transition={{ duration: 1, delay: 0, ease: [0, 0.71, 0.2, 1.01] }}
+          href={blog.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={`Read more about ${blog.title}`}
+          className="w-full"
         >
-          {/* Thumbnail */}
-          <div className="flex-shrink-0 mr-4">
-            <img
-              src={blog.thumbnail}
-              alt={blog.title}
-              style={{
-                borderWidth: "0.5px",
-                borderColor: "#B0B3B8",
-                boxShadow: "0 2px 5px rgba(0, 255, 170, 0.637)",
-              }}
-              className="object-fit mx-auto"
-            />
-          </div>
-
-          {/* Title and Tags */}
-          <div className="flex-grow">
-            <a
-              href={blog.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-xl text-color"
-            >
-              {blog.title}
-            </a>
-            <div className="mt-2 flex items-center gap-2 text-gray-500">
-              <span className="text-sm">Rene Ortega</span> | <span className="text-xs"> 2023 </span>
+          <motion.div
+            className="p-4 bg-slate-800 rounded-xl shadow-lg flex flex-col md:flex-row items-center border-b border-slate-700"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            whileHover={{ scale: 1.02, boxShadow: "0 2px 10px rgba(0, 255, 170, 0.637)" }}
+            transition={{ duration: 1 }}
+          >
+            {/* Thumbnail */}
+            <div className="flex-shrink-0 mb-4 md:mb-0 md:mr-4">
+              <img
+                src={blog.thumbnail}
+                alt={blog.title}
+                className="object-fit mx-auto"
+                style={{
+                  borderWidth: "0.5px",
+                  borderColor: "#B0B3B8",
+                  boxShadow: "0 2px 5px rgba(0, 255, 170, 0.637)",
+                }}
+              />
             </div>
-            {/* Tags Pills */}
-            <div className="mt-2 flex flex-wrap">
-              {blog.tags
-                .replace(/"/g, "")
-                .split(",")
-                .filter((tag) => tag.trim() !== "")
-                .map((tag, index) => (
-                  <span
-                    key={index}
-                    className="px-2 py-1 mr-2 mt-2 text-color rounded-full text-sm bg-teal-400/10"
-                  >
+
+            {/* Title and Tags */}
+            <div className="flex-grow">
+              <span className="text-xl text-color">{blog.title}</span>
+              <div className="mt-2 flex items-center gap-2 text-gray-500">
+                <span className="text-sm">Rene Ortega</span> | <span className="text-xs">2023</span>
+              </div>
+              {/* Tags Pills */}
+              <div className="mt-2 flex flex-wrap">
+                {blog.tags.split(",").filter(tag => tag.trim() !== "").map((tag, index) => (
+                  <span key={index} className="px-2 py-1 mr-2 mt-2 text-color rounded-full text-sm bg-teal-400/10">
                     {tag.trim()}
                   </span>
                 ))}
+              </div>
             </div>
-          </div>
-        </motion.div>
+          </motion.div>
+        </a>
       ))}
     </div>
   );
 };
 
 export default Blogs;
-
